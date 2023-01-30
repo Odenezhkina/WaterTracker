ea9f2146db1a3f89a6755e60e4691f7f3cbe9a30
Odenezhkina <66617436+Odenezhkina@users.noreply.github.com>
2023-01-30 15:48:29 +0300

    `DI` init: hilt was added, database module was added too

---
+++ app/src/main/java/com/study/watertracker/di/DatabaseModule.kt
@@ -0,0 +1,27 @@
+package com.study.watertracker.di
+
+import android.app.Application
+import androidx.room.Room
+import com.study.watertracker.data.WaterIntakeDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun providesWaterIntakeDatabase(app: Application): WaterIntakeDatabase =
+        Room.databaseBuilder(app, WaterIntakeDatabase::class.java, "water_intake_database")
+            .fallbackToDestructiveMigration()
+            .build()
+
+    @Provides
+    @Singleton
+    fun providesWaterIntakeDao(database: WaterIntakeDatabase) = database.waterIntakeDao()
+
+}
